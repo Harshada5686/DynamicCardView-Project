8ef6ce21fef1bd733053c4f62cd6ca2783d0b7ca
Harshada5686 <harshadawagh5686@gmail.com>
2024-01-20 20:26:07 +0530

    Create new file

---
+++ javascript/index.js
@@ -0,0 +1,51 @@
+const cardData =[
+    
+    {
+        heading:'card 1',
+        body:'this is card body1'
+    },
+    {
+        heading:'card 2',
+        body:'this is card body2'
+    },{
+        heading:'card 3',
+        body:'this is card body3'
+    },
+    {
+        heading:'card 4',
+        body:'this is card body4'
+    },
+    {
+        heading:'card 5',
+        body:'this is card body5'
+    },
+    {
+        heading:'card 6',
+        body:'this is card body6'
+    },
+    {
+        heading:'card 7',
+        body:'this is card body7'
+    },
+    {
+        heading:'card 8',
+        body:'this is card body8'
+    },
+]
+
+const postContainer = document.querySelector('.card-container');
+
+const postMethods = ()=>{
+    cardData.map((postData)=>{
+        // console.log(postData)
+        const postElement = document.createElement('div');
+        postElement.classList.add('card');
+        postElement.innerHTML=`
+        <h3 class="card-heading">${postData.heading}</h3>
+        <p class="card-body">${postData.body}</p>
+        
+        `
+        postContainer.appendChild(postElement)
+    })
+}
+postMethods()
