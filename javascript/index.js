26589c3eaf3dcf364ef645d09d3414971e21f8da
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-02-09 20:33:55 +0530

    pull change

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,8 @@
+let productsArray = [{}];
 const cardData =[
-    
     {
         heading:'card 1',
-        body:'this is card body1'
+        body:'this is card body1',
     },
     {
         heading:'card 2',
@@ -31,13 +31,51 @@ const cardData =[
         heading:'card 8',
         body:'this is card body8'
     },
-]
+    {
+        heading:'card 1',
+        body:'this is card body1',
+    },
+    {
+        heading:'card 2',
+        body:'this is card body2'
+    },{
+        heading:'card 3',
+        body:'this is card body3'
+    },
+    {
+        heading:'card 4',
+        body:'this is card body4'
+    },
+    {
+        heading:'card 5',
+        body:'this is card body5'
+    },
+];
 
 const postContainer = document.querySelector('.card-container');
 
+const getData = () =>{
+    fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(data => {
+                // Store the data in the productsArray
+                productsArray = data;
+        
+                // Now you can use productsArray as needed
+                
+                console.log(productsArray)
+            })
+            console.log("after")
+            console.log(productsArray)
+            console.log(productsArray);
+            console.log("hi")
+}
+
+
 const postMethods = ()=>{
     cardData.map((postData)=>{
-        // console.log(postData)
+        console.log("pdata:"+postData)
+
         const postElement = document.createElement('div');
         postElement.classList.add('card');
         postElement.innerHTML=`
@@ -49,3 +87,4 @@ const postMethods = ()=>{
     })
 }
 postMethods()
+getData();
